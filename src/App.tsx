fb19671d970ae75e2d0da8a426181d41ae81f012
Mikhail Kuznetsov <mk.mikhail.k@gmail.com>
2024-06-26 22:49:42 +0300

    => init

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+export function App() {
+  return (
+    <div
+      style={{
+        alignItems: 'center',
+        display: 'flex',
+        height: '100vh',
+        justifyContent: 'center',
+        width: '100vw',
+      }}
+    >
+      💩💩💩
+    </div>
+  )
+}
