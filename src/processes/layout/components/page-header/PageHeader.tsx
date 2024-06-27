aa3f40dc41f4df79696ab066130b57ff084f73a2
Dmitriy-Zarechnev <zarechnev5021@gmail.com>
2024-06-27 16:59:11 +0300

    27.06 - button header - 2

--- src/processes/layout/components/page-header/PageHeader.tsx
+++ src/processes/layout/components/page-header/PageHeader.tsx
@@ -23,13 +23,13 @@ export const PageHeader = forwardRef<ElementRef<'header'>, PageHeaderProps>(({ d
           <img alt={'Project Picture'} className={s.projectPicture} src={logo} />
         </div>
 
-        <Button as={'a'} classNameText={s.buttonText} variant={'link'}>
+        <Button as={'a'} classNameText={s.buttonText} variant={'secondary'}>
           ГЛАВНАЯ
         </Button>
-        <Button as={'a'} classNameText={s.buttonText} variant={'link'}>
+        <Button as={'a'} classNameText={s.buttonText} variant={'secondary'}>
           Магазин 3D решений
         </Button>
-        <Button as={'a'} classNameText={s.buttonText} variant={'link'}>
+        <Button as={'a'} classNameText={s.buttonText} variant={'secondary'}>
           Контакты
         </Button>
 
