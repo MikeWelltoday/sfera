cb0e4b5d7d79aadd11d8fc4bdd1f5c5ae6a9902a
Dmitriy-Zarechnev <zarechnev5021@gmail.com>
2024-06-28 20:01:42 +0300

    28.06 - stories - 1

--- src/pages/private/_components/back-to-decks/BackToDecks.stories.tsx
+++ src/pages/private/_components/back-to-decks/BackToDecks.stories.tsx
@@ -1,6 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react'
 
-import { StorybookDecorator } from '@/services/StorybookDecorator'
+import { StorybookDecorator } from '@/state/StorybookDecorator'
 
 import { BackToDecks } from './BackToDecks'
 
