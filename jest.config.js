608c2ef19aa708ad8cebf7db2b11190829b907a3
Mikhail Kuznetsov <mk.mikhail.k@gmail.com>
2024-06-29 17:58:30 +0300

    => 1st version => tests are fixed

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,15 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 export default {
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    '\\.(jpg|jpeg|png|gif|webp|svg)$': '<rootDir>/__mocks__/fileMock.js',
+  } /* 🟧 обработка картинок при тестах */,
   preset: 'ts-jest',
   testEnvironment: 'node',
-};
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+  transformIgnorePatterns: [
+    'node_modules/(?!@ngrx|(?!deck.gl)|ng-dynamic)',
+  ] /* 🟧 обработка картинок при тестах */,
+}
